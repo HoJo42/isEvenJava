a929526a74ffbfba823300c7ba87994a770d04e8
HowardWalz <howardwalz@gmail.com>
2022-10-01 09:05:34 -0700

    Program now continously loops, closes because of an exception thrown. Will need to fix.

--- isEven.java
+++ isEven.java
@@ -12,11 +12,19 @@ public class isEven {
     }
 
     public static void main(String[] args) {
+        boolean i = true;
         Scanner myScanner = new Scanner(System.in); //create scanner
+    do {
+        
         System.out.println("Enter a number");
 
         int testNumber = myScanner.nextInt();
         isNumEven(testNumber);
-        myScanner.close();
+        String exitCase = myScanner.nextLine();
+        if (exitCase.toLowerCase() == "exit"){
+            i = false;
+        }
+        //myScanner.close();
+    } while (i);
     }
 }
